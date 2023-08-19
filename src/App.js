9c3117de6e5ece48562f3dc3c8346c0a1a765eeb
catzilla <catzillaorz@outlook.com>
2023-08-20 07:57:25 +0800

    feat: build layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats/>
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏖️ Far Away</h1>
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h31>What do you need for your 😍 trip?</h31>
+    </div>
+  )
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>
+}
+
+function Stats() {
+  return <footer className="stats">
+    <em> 🧰 You hava X items on you list, and you already packed X(x%) </em>
+  </footer>
+}
