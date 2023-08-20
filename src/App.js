0068ae67e5cb5be677697c7797cd3a8163a19790
catzilla <catzillaorz@outlook.com>
2023-08-20 21:51:18 +0800

    feat: update Items Complex immutablely

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ const initialItems = [
 
 export default function App() {
     const [items, setItems] = useState(initialItems);
+
     function handleAddItems(item) {
         setItems((items) => [...items, item]);
     }
+
     function handleDeleteItem(id) {
         setItems((items) => items.filter((item) => item.id !== id));
     }
+
+    function handleToggleItem(id) {
+        setItems((items) => items.map((item) => (item.id === id ? { ...item, packed: !item.packed } : item)));
+    }
+
     return (
         <div className="app">
             <Logo />
@@ -21,6 +28,7 @@ export default function App() {
             <PackingList
                 items={items}
                 onDeleteItem={handleDeleteItem}
+                onToggleItems={handleToggleItem}
             />
             <Stats />
         </div>
@@ -76,7 +84,7 @@ function Form({ onAddItems }) {
     );
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItems }) {
     return (
         <div className="list">
             <ul>
@@ -85,6 +93,7 @@ function PackingList({ items, onDeleteItem }) {
                         item={item}
                         key={item.id}
                         onDeleteItem={onDeleteItem}
+                        onToggleItems={onToggleItems}
                     />
                 ))}
             </ul>
@@ -92,9 +101,13 @@ function PackingList({ items, onDeleteItem }) {
     );
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItems }) {
     return (
         <li>
+            <input
+                type="checkbox"
+                onChange={() => onToggleItems(item.packed)}
+            />
             <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
                 {item.quantity}
                 {item.description}{' '}
