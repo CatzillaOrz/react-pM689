58015651966529879ccffa884949c72751f92f6b
catzilla <catzillaorz@outlook.com>
2023-08-20 23:11:08 +0800

    feat: caculate statistics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import { useState } from 'react';
 
-const initialItems = [
-    { id: 1, description: 'Passports', quantity: 2, packed: false },
-    { id: 2, description: 'Socks', quantity: 12, packed: true },
-    { id: 3, description: 'Charger', quantity: 1, packed: false },
-];
-
 export default function App() {
-    const [items, setItems] = useState(initialItems);
+    const [items, setItems] = useState([]);
 
     function handleAddItems(item) {
         setItems((items) => [...items, item]);
@@ -30,7 +24,7 @@ export default function App() {
                 onDeleteItem={handleDeleteItem}
                 onToggleItems={handleToggleItem}
             />
-            <Stats />
+            <Stats items={items} />
         </div>
     );
 }
@@ -106,7 +100,7 @@ function Item({ item, onDeleteItem, onToggleItems }) {
         <li>
             <input
                 type="checkbox"
-                onChange={() => onToggleItems(item.packed)}
+                onChange={() => onToggleItems(item.id)}
             />
             <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
                 {item.quantity}
@@ -117,10 +111,20 @@ function Item({ item, onDeleteItem, onToggleItems }) {
     );
 }
 
-function Stats() {
+function Stats({ items }) {
+    if (!items.length)
+        return (
+            <p className="stats">
+                <em>Start adding some items to your packing list 🚀</em>
+            </p>
+        );
+
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = Math.round((numPacked / numItems) * 100);
     return (
         <footer className="stats">
-            <em> 🧰 You hava X items on you list, and you already packed X(x%) </em>
+            <em>{percentage === 100 ? 'You got everything! Ready to go ✈️' : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}</em>
         </footer>
     );
 }
